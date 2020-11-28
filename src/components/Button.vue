7510026ce5cc26f2c4f38fc5cfd811bb004956bf
canxiang.zhao <canxiang.zhao@seek4paws.com>
2020-11-28 16:45:31 +0800

    add Button component

---
+++ src/components/Button.vue
@@ -0,0 +1,42 @@
+<template>
+  <button
+    :class="'i-button-' + size"
+    :disabled="disabled"
+    style="backgroundColor: yellow; height: 30px;"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "i-button",
+  inheritAttrs: false,
+  props: {
+    size: {
+      validator(value) {
+        return ["small", "large", "default"].includes(value);
+      }
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  created() {
+    console.log(this.$attrs);
+  }
+};
+</script>
+
+<style scoped>
+.i-button-default {
+  width: 70px;
+}
+.i-button-small {
+  width: 50px;
+}
+.i-button-large {
+  width: 90px;
+}
+</style>
